cd25fd3b35d6b8ba94d0f62e90ecfcbaa75c24f6
Jaedson Barbosa Serafim <jaedson33@gmail.com>
2021-02-21 21:21:10 -0300

    add scan CNPJ e base de formulários.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,10 +2,11 @@ import * as functions from "firebase-functions";
 import * as firebase from 'firebase-admin'
 const cors = require('cors')({ origin: true, allowedHeaders: 'Authorization' });
 firebase.initializeApp()
-// const db = firebase.firestore()
+const db = firebase.firestore()
 // const FieldValue = firebase.firestore.FieldValue
+const FieldPath = firebase.firestore.FieldPath
 
-async function getUser(req: any) {
+async function getUser(req: any): Promise<firebase.auth.DecodedIdToken | undefined> {
 	const authorizationHeader = req.headers.authorization || '';
 	const components = authorizationHeader.split(' ');
 	const idToken = components.length > 1 ? components[1] : '';
@@ -18,14 +19,42 @@ async function getUser(req: any) {
 	return undefined
 }
 
-export const test = functions.https.onRequest((req, res) => cors(req, res, async () => {
+export const scanUsuario = functions.https.onRequest((req, res) => cors(req, res, async () => {
 	const user = await getUser(req);
 	if (!user) {
 		// Usuário não foi encontrado, então apenas se rejeita a requisição.
 		res.sendStatus(401)
-	} else {
-		res.status(200).send(user)
+		return
 	}
+	const empresas = await db.collectionGroup('usuarios').where(FieldPath.documentId(), '==', user.sub).get()
+	if (empresas.empty) {
+		res.status(200).send('Nenhuma empresa')
+		return
+	}
+	
+}))
+
+export const scanCNPJ = functions.https.onRequest((req, res) => cors(req, res, async () => {
+	const user = await getUser(req);
+	if (!user) {
+		// Usuário não foi encontrado, então apenas se rejeita a requisição.
+		res.sendStatus(401)
+		return
+	}
+	const cnpj = req.query.cnpj
+	if (!cnpj) {
+		res.sendStatus(400)
+		return
+	}
+	const empresas = await db.collection('empresas').where('CNPJ', '==', cnpj).select().limit(1).get()
+	if (empresas.empty) {
+		res.status(200).send('Empresa não existe')
+		return
+	}
+	const empresa = empresas.docs[0]
+	const usuario = await empresa.ref.collection('usuarios').doc(user.sub).get()
+	if (usuario.exists) res.status(200).send(usuario.data())
+	else res.status(200).send('Usuário não existe')
 }))
 
 /*
--- public-src/welcome.ts
+++
@@ -1,7 +0,0 @@
-import db from './db'
-
-db.create().then(v => {
-    const addOperation = v.emitentes.add({ 'nome': 'Jaedson' }, db.autoId)
-    addOperation.onsuccess = () => console.log(addOperation.result)
-    addOperation.onerror = (e) => console.error(e)
-}).catch(v => alert(v))
