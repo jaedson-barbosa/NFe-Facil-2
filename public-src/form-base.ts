ee9b27ee59366eee101df16d8c4ad2ac03b8065f
Jaedson Barbosa Serafim <jaedson33@gmail.com>
2021-03-11 22:08:03 -0300

    Refeito create view.

--- public-src/form-base.ts
+++ public-src/form-base.ts
@@ -37,15 +37,15 @@ export function defaultFormSubmit(e: Event, onSubmit: (data: any) => void) {
 }
 
 export function initializeForm(
-    customHeaders: {name: string, header: string}[],
+    customHeaders: { name: string, header: string }[],
     customRequireds: string[],
     sourceGetter: (source: any) => any,
     formParent: HTMLElement,
     additionalBody?: HTMLDivElement): HTMLFormElement {
-    
+
     function createView(parentView: HTMLElement, field: any, parentTags: string[], tag: string = '', ignorarAnaliseOcorrencia: boolean = false): void {
         const getDocumentation = (v: any): string => v.annotation?.documentation?._text ?? 'VAZIO'
-        const isRequired = (v : any): boolean => (v._attributes?.minOccurs ?? 1) > 0 || customRequireds.includes(v._attributes.name)
+        const isRequired = (v: any): boolean => (v._attributes?.minOccurs ?? 1) > 0 || customRequireds.includes(v._attributes.name)
         const updateInput = (v: any, input?: HTMLElement): HTMLElement | undefined => {
             const fieldRestriction = v.simpleType?.restriction
             const baseType = v._attributes?.type ?? fieldRestriction?._attributes.base
@@ -232,7 +232,7 @@ export function initializeForm(
                     paisLabel.innerText = 'País'
                     const datalist = document.createElement('datalist')
                     datalist.innerHTML = paises.map(
-                            k => `<option>${k.nome}</option>`).join('')
+                        k => `<option>${k.nome}</option>`).join('')
                     paisSmartSelect.setAttribute('list', datalist.id = createId())
                     paisSmartSelect.onchange = () => {
                         try {
@@ -269,8 +269,7 @@ export function initializeForm(
         }
     }
 
-    const complex = nfeSchema.schema.complexType[0].sequence.element[0]['complexType']
-    const elementosNFe = complex['sequence']['element']
+    const elementosNFe = nfeSchema.schema.complexType[0].sequence.element[0]['complexType']['sequence']['element']
     const targetSource = sourceGetter(elementosNFe);
     const form = document.createElement('form')
     formParent.appendChild(form)
@@ -290,4 +289,301 @@ export function initializeForm(
         (input as any).setCustomValidity('Selecione um valor.')
     })
     return form
+}
+
+const customHeaders: { name: string, header: string }[] = []
+
+interface baseFormElement {
+    generate: (parent: HTMLElement) => void
+}
+
+export class divFormElement implements baseFormElement {
+    private element: HTMLDivElement
+
+    constructor(el: HTMLDivElement) {
+        this.element = el
+    }
+
+    public generate(parent: HTMLElement) {
+        parent.appendChild(this.element)
+    }
+}
+
+abstract class inputFormElement implements baseFormElement {
+    private name: string[]
+    private documentation: string
+    private required: boolean
+
+    constructor(name: string[], documentation: string, required: boolean) {
+        this.name = name
+        this.documentation = documentation
+        this.required = required
+    }
+
+    public abstract generate(parent: HTMLElement);
+
+    protected updateBaseProps(input: HTMLSelectElement | HTMLInputElement) {
+        input.name = this.name.join('.')
+        input.title = this.documentation
+        input.required = this.required
+    }
+
+    protected insertLabel(input: HTMLSelectElement | HTMLInputElement) {
+        const label = document.createElement('label')
+        label.htmlFor = input.id = createId()
+        const customHeader = customHeaders.find(v => (input as any).name.includes(v.name))?.header
+        label.innerText = customHeader ?? this.documentation.split('\n')[0].split('.')[0].split('(')[0]
+        input.parentElement.insertBefore(label, input)
+    }
+}
+
+export class selectFormElement extends inputFormElement {
+    private options: { value: string, text: string }[]
+
+    constructor(
+        name: string[],
+        documentation: string,
+        required: boolean,
+        options: { value: string, text: string }[]) {
+        super(name, documentation, required)
+        this.options = options
+    }
+
+    public generate(parent: HTMLElement) {
+        const select = document.createElement('select')
+        const options = this.options.map(v => `<option value="${v.value}">${v.text}</option>`)
+        select.innerHTML = options.join('')
+        this.updateBaseProps(select)
+        parent.appendChild(select)
+        this.insertLabel(select)
+    }
+}
+
+export class textFormElement extends inputFormElement {
+    private options: {
+        pattern?: string,
+        minLength?: number,
+        maxLength?: number
+    }
+
+    constructor(
+        name: string[],
+        documentation: string,
+        required: boolean,
+        options: {
+            pattern?: string,
+            minLength?: number,
+            maxLength?: number
+        }) {
+        super(name, documentation, required)
+        this.options = options
+    }
+
+    public generate(parent: HTMLElement) {
+        const text = document.createElement('input')
+        text.type = 'text'
+        if (this.options.pattern) text.pattern = this.options.pattern
+        if (this.options.minLength) text.minLength = this.options.minLength
+        if (this.options.maxLength) text.maxLength = this.options.maxLength
+        this.updateBaseProps(text)
+        parent.appendChild(text)
+        this.insertLabel(text)
+    }
+}
+
+export class hiddenFormElement extends inputFormElement {
+    private value: string
+
+    constructor(
+        name: string[],
+        documentation: string,
+        required: boolean,
+        value?: string) {
+        super(name, documentation, required)
+        this.value = value
+    }
+
+    public generate(parent: HTMLElement) {
+        const hidden = document.createElement('input')
+        hidden.type = 'hidden'
+        hidden.value = this.value
+        this.updateBaseProps(hidden)
+        parent.appendChild(hidden)
+    }
+}
+
+export class fieldsetFormElement implements baseFormElement {
+    private legend: string
+    private children: baseFormElement[]
+
+    constructor(children: baseFormElement[], legend?: string) {
+        this.children = children
+        this.legend = legend
+    }
+
+    public generate(parent: HTMLElement) {
+        const content = document.createElement('fieldset')
+        if (this.legend) {
+            const legend = document.createElement('legend')
+            legend.innerText = this.legend
+            content.appendChild(legend)
+        }
+        this.children.forEach(v => v.generate(content))
+        parent.appendChild(content)
+    }
+}
+
+export class choiceFormElement implements baseFormElement {
+    private options: string[]
+    private contentGetter: (optionIndex: number) => baseFormElement
+
+    constructor(
+        options: string[],
+        contentGetter: (optionIndex: number) => baseFormElement) {
+        this.options = options
+        this.contentGetter = contentGetter
+    }
+
+    public generate(parent: HTMLElement) {
+        const select = document.createElement('select')
+        const div = document.createElement('div')
+        const options = this.options.map(v => `<option>${v}</option>`)
+        select.innerHTML = options.join('')
+        select.onchange = () => {
+            const index = select.selectedIndex
+            const content = this.contentGetter(index)
+            div.innerHTML = ''
+            content.generate(div)
+        }
+        parent.appendChild(select)
+        parent.appendChild(div)
+    }
+}
+
+export class defaultForm {
+    static elementosNFe = nfeSchema.schema.complexType[0].sequence.element[0]['complexType']['sequence']['element']
+
+    private elements: baseFormElement[]
+
+    public addElement(el: baseFormElement) {
+        this.elements.push(el)
+    }
+
+    static generateElement(
+        rootField: any,
+        customRequireds: string[]): baseFormElement {
+
+        function isRequired(v: any): boolean {
+            const fromSchema = (v._attributes?.minOccurs ?? 1) > 0
+            const fromCode = customRequireds.includes(v._attributes.name)
+            return fromSchema || fromCode
+        }
+
+        function getDocumentation(v: any) { 
+            return v.annotation?.documentation?._text ?? 'VAZIO'
+        }
+
+        function createInput(field: any, parentTags: string[]): baseFormElement {
+            const fieldRestriction = field.simpleType?.restriction
+            const baseType = field._attributes?.type ?? fieldRestriction?._attributes.base
+            const findType = (v: any) => v._attributes.name == baseType
+            const otherRestrictions: any = (simpleTypes.find(findType) as any)?.restriction
+            if (!otherRestrictions) {
+                const complexType = complexTypes.find(findType)
+                if (!complexType) throw new DOMException('Invalid field')
+                const newField = { ...field, sequence: complexType.sequence }
+                return createFieldset(newField, parentTags)
+            }
+            if (otherRestrictions?._attributes.base != 'string') {
+                console.log(otherRestrictions)
+                throw new DOMException('The other restrictions have a invalid base.')
+            }
+            const enumeration = fieldRestriction?.enumeration ?? otherRestrictions?.enumeration
+            if (!enumeration) {
+                const name = [...parentTags, field._attributes.name]
+                const documentation = getDocumentation(field)
+                const required = isRequired(field)
+                const getProp = (el: string) => {
+                    const fieldProp = fieldRestriction?.[el]?._attributes.value
+                    const otherProp = otherRestrictions?.[el]?._attributes.value
+                    return fieldProp ?? otherProp
+                }
+                return new textFormElement(name, documentation, required, {
+                    pattern: getProp('pattern'),
+                    minLength: getProp('minLength'),
+                    maxLength: getProp('maxLength')
+                })
+            }
+            const name = [...parentTags, field._attributes.name]
+            const required = isRequired(field)
+            const documentation = getDocumentation(field)
+            if (!('length' in enumeration)) {
+                const val = enumeration._attributes.value
+                return new hiddenFormElement(name, documentation, required, val)
+            }
+            const val0 = enumeration[0]._attributes.value
+            const val1 = enumeration[1]._attributes.value.toUpperCase()
+            if (enumeration.length == 2 && val0.toUpperCase() == val1)
+                return new hiddenFormElement(name, documentation, required, val0)
+            if (enumeration.length <= 2)
+                throw new DOMException('Invalid enumeration length')
+            const values = (enumeration as any[]).map(v => v._attributes.value)
+            const isDesc = (v: string, line: string) => line.includes(v + ' – ')
+            const lines = documentation.split('\n')
+            const hasText = values.every(v => lines.some(l => isDesc(v, l)))
+            const options = values.map(v => {
+                return {
+                    value: v,
+                    text: hasText ? lines.find((l: string) => isDesc(v, l)) : v
+                }
+            })
+            return new selectFormElement(name, documentation, required, options)
+        }
+
+        function createChoice(field: any, parentTags: string[]): baseFormElement {
+            const elements = field['element'] as any[]
+            const options = elements.map(v => getDocumentation(v))
+            return new choiceFormElement(options, i => createInput(elements[i], parentTags))
+        }
+
+        function createFieldset(field: any, parentTags: string[]): baseFormElement {
+            const legend = getDocumentation(field)
+            const tags = [...parentTags, field._attributes.name]
+            if (field.element) {
+                const elements = field.element as any[]
+                const inputs = elements.map(v => createInput(v, tags))
+                return new fieldsetFormElement(inputs, legend)
+            } else if (field.complexType || field.sequence) {
+                const sequence = field.complexType.sequence ?? field.sequence
+                const pureFields = Object.entries(sequence)
+                const fields = pureFields.flatMap(v => analyseTag(v[0], v[1], tags))
+                return new fieldsetFormElement(fields, legend)
+            } else throw new DOMException('Invalid tag for a fieldset')
+        }
+
+        function analyseTag(tag: string, field: any, parentTags: string[]): baseFormElement[] {
+            switch (tag) {
+                case 'choice':
+                    return [createChoice(field, parentTags)]
+                case 'element':
+                    const elements = field as any[]
+                    return elements.map(v => createInput(v, parentTags))
+                default:
+                    if (!tag || tag == 'sequence')
+                        return [createFieldset(field, parentTags)]
+                    throw new DOMException('Invalid tag')
+            }
+        }
+
+        return createFieldset(rootField, [])
+    }
+
+    public generateForm() {
+        const form = document.createElement('form')
+        this.elements.forEach(v => v.generate(form))
+        const submit = document.createElement('input')
+        submit.type = 'submit'
+        form.appendChild(submit)
+        return form
+    }
 }
