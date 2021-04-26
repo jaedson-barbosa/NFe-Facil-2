35d3493b2c2fe84483ad12c2f14bb01db55fca27
Jaedson Barbosa Serafim <jaedson33@gmail.com>
2021-04-26 18:27:23 -0300

    Adicionada importação de dados base.

--- functions/src/importacao.ts
+++ functions/src/importacao.ts
@@ -1,5 +1,5 @@
 import { toJson } from 'xml2json'
-import { db, FieldValue, onLoggedRequest } from './core'
+import { db, onLoggedRequest } from './core'
 
 // function addPrefix(obj: any) {
 //     Object.entries(obj).forEach(
@@ -20,26 +20,121 @@ function removePrefix(obj: any) {
   return obj
 }
 
+function getLastItens<T extends { lastUpdate: Date }>(
+  itens: T[],
+  keyGetter: (v: T) => string
+) {
+  return itens
+    .reduce((p, c) => {
+      const key: string = keyGetter(c)
+      const last = p.find((v) => v[0] == key)
+      if (last) {
+        if (last[1].lastUpdate < c.lastUpdate) {
+          last[1] = c
+        }
+      } else p.push([key, c])
+      return p
+    }, [] as [string, T][])
+    .map((v) => v[1])
+}
+
 //Adicionar importação de dados base a partir da NFe, por enquanto sem processamento.
 export const importar = onLoggedRequest(
   async (user, res, empresaRef, empresa, body) => {
     const notasCollection = empresaRef.collection('notas')
-    res.status(200).send(
-      await (body.xmls as string[])
-        .map((v) => {
-          const root = toJson(v, { object: true, reversible: true }) as any
-          return root?.NFe || root?.nfeProc
-            ? {
-                json: removePrefix((root.NFe ?? root.nfeProc.NFe).infNFe),
-                xml: v,
-                emitido: !!root.nfeProc,
-                lastUpdate: FieldValue.serverTimestamp(),
-              }
-            : undefined
+    let batch = db.batch()
+    const notas = (body.xmls as string[])
+      .map((v) => {
+        const root = toJson(v, { object: true, reversible: true }) as any
+        if (!root?.NFe && !root?.nfeProc) return undefined
+        const json = removePrefix((root.NFe ?? root.nfeProc.NFe).infNFe)
+        return {
+          json,
+          xml: v,
+          emitido: !!root.nfeProc,
+          lastUpdate: new Date(json.ide.dhEmi),
+        }
+      })
+      .filter((v) => v)
+      .map((v) => v!)
+    batch = notas.reduce((p, c) => p.create(notasCollection.doc(), c), batch)
+
+    const dadosCollection = empresaRef.collection('dados')
+    const clientes = getLastItens(
+      notas
+        .filter((v) => {
+          const d = v.json.dest
+          return d && d.xNome && d.enderDest
         })
-        .filter((v) => v)
-        .reduce((p, c) => p.create(notasCollection.doc(), c), db.batch())
-        .commit()
+        .map((v) => {
+          return {
+            dest: v.json.dest,
+            lastUpdate: v.lastUpdate,
+          }
+        }),
+      (v) => v.dest.CPF ?? v.dest.CNPJ
     )
+    batch = clientes.reduce((p, c) => p.create(dadosCollection.doc(), c), batch)
+
+    const camposProdutos = [
+      'xProd',
+      'cProd',
+      'EXTIPI',
+      'uCom',
+      'uTrib',
+      'CFOP',
+      'NCM',
+      'vUnCom',
+      'vUnTrib',
+    ]
+    const produtos = getLastItens(
+      notas
+        .map((j) => {
+          const dets: any[] = Array.isArray(j.json.det)
+            ? j.json.det
+            : [j.json.det]
+          return dets.map((v: any) => {
+            const prod = v.prod
+            return {
+              prod: {
+                cEAN: prod.cEAN ?? 'SEM GTIN',
+                cEANTrib: prod.cEANTrib ?? 'SEM GTIN',
+                ...camposProdutos.reduce((p, c) => {
+                  p[c] = prod[c]
+                  return p
+                }, {} as any),
+              },
+              lastUpdate: j.lastUpdate,
+            }
+          })
+        })
+        .reduce((p, c) => {
+          p.push(...c)
+          return p
+        }, []),
+      (v) => `${v.prod.cProd}${v.prod.xProd}`
+    )
+    batch = produtos.reduce((p, c) => p.create(dadosCollection.doc(), c), batch)
+
+    const motoristas = getLastItens(
+      notas
+        .filter((v) => {
+          const t = v.json.transp.transporta
+          return t && (t.CPF || t.CNPJ)
+        })
+        .map((v) => {
+          return {
+            transporta: v.json.transp.transporta,
+            lastUpdate: v.lastUpdate,
+          }
+        }),
+      (v) => v.transporta.CPF ?? v.transporta.CNPJ
+    )
+    batch = motoristas.reduce(
+      (p, c) => p.create(dadosCollection.doc(), c),
+      batch
+    )
+    const commitRes = await batch.commit()
+    res.status(200).send({ newItens: commitRes.length })
   }
 )
