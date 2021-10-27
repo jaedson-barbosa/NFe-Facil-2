483c9443886494107a874a314c219f6e059de579
Jaedson <jaedson33@gmail.com>
2021-10-27 09:42:03 -0300

    Corrigida exibição permanente do encerrante.

--- app/src/parts-produto/Encerrante.svelte
+++ app/src/parts-produto/Encerrante.svelte
@@ -15,23 +15,35 @@
     <option value={true}>Sim</option>
   </select>
 </label>
-<label>
-  Numero do bico utilizado no abastecimento
-  <input type="number" step="1" bind:value={encerrante.nBico} required />
-</label>
-<label>
-  Numero de identificação do tanque
-  <input type="number" step="1" bind:value={encerrante.nTanque} required />
-</label>
-<label>
-  Valor do encerrante no ínicio do abastecimento
-  <input type="number" step="0.001" bind:value={encerrante.vEncIni} required />
-</label>
-<label>
-  Valor do Encerrante no final do abastecimento
-  <input type="number" step="0.001" bind:value={encerrante.vEncFin} required />
-</label>
-<label>
-  <i>Numero da bomba</i>
-  <input type="number" bind:value={encerrante.nBomba} />
-</label>
+{#if encerrante}
+  <label>
+    Numero do bico utilizado no abastecimento
+    <input type="number" step="1" bind:value={encerrante.nBico} required />
+  </label>
+  <label>
+    Numero de identificação do tanque
+    <input type="number" step="1" bind:value={encerrante.nTanque} required />
+  </label>
+  <label>
+    Valor do encerrante no ínicio do abastecimento
+    <input
+      type="number"
+      step="0.001"
+      bind:value={encerrante.vEncIni}
+      required
+    />
+  </label>
+  <label>
+    Valor do Encerrante no final do abastecimento
+    <input
+      type="number"
+      step="0.001"
+      bind:value={encerrante.vEncFin}
+      required
+    />
+  </label>
+  <label>
+    <i>Numero da bomba</i>
+    <input type="number" bind:value={encerrante.nBomba} />
+  </label>
+{/if}
